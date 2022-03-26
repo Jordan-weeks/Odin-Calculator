a64fe3784b571ab4b8ce305e8f03d57ec482b1ac
Jordan <jdweeks2010@gmail.com>
2022-03-25 20:12:32 -0700

    added opperator functions

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Basic calculator functions
-const plus = (x,y) => (x+y); 
+const plus = (x,y) => (Number(x)+ Number(y)); 
 const subtract = (x,y ) => (x-y);
 const multiply = (x,y) => (x*y);
 const divide = (x,y) => (x/y);
 let displayValue = ""
 let storedValue = ""    
-let storedOperator = ""   
+let storedOperator = "" 
+let display = document.querySelector(".display");  
 const updateDisplay = (number) => {
-    let display = document.querySelector(".display");
     displayValue = displayValue.concat(number)
     console.log(displayValue)
     display.innerHTML = displayValue 
@@ -18,8 +18,21 @@ const callOperator = (operator) =>{
     storedOperator = operator
     console.log(storedOperator)
 }
-const operate = (numberOne, operator, numberTwo) =>
-    operator(numberOne, numberTwo);
+const operate = (numberOne, operator, numberTwo) => {
+    if (operator == "plus")
+    displayValue = (plus(numberOne, numberTwo));
+    display.innerHTML = displayValue; 
+    if (operator == "subtract") 
+    displayValue = (subtract(numberOne, numberTwo));
+    display.innerHTML = displayValue;
+    if (operator == "multiply") 
+    displayValue = (multiply(numberOne, numberTwo));
+    display.innerHTML = displayValue;
+    if (operator == "divide") 
+    displayValue = (divide(numberOne, numberTwo));
+    display.innerHTML = displayValue;
+    
+}
 //number event listeners
 let numberButtons = document.querySelectorAll(".number")
 numberButtons.forEach(item => item.addEventListener("click", e =>updateDisplay(e.target.innerHTML)))
@@ -30,7 +43,11 @@ opperatorButtons.forEach(item =>item.addEventListener("click", (e) => callOperat
  ))
 
 //equals event listener
-let equals = document.querySelector("#equals").addEventListener("click", () => operate(storedValue))
-// const buttons = document.querySelectorAll("button");
+let equals = document.querySelector("#equals").addEventListener("click", () => operate(storedValue,storedOperator,displayValue))
 
-// buttons.forEach(button in buttons){addEventListener}
+// Clear button
+let clear = document.querySelector("#clear").addEventListener("click", () =>( 
+displayValue = "",
+storedValue = "",    
+storedOperator = "",
+updateDisplay(storedValue)))
